0ca0addb7268ee34b8ee5337ebf47ac51c68af87
João Lucas Lourenço <jao.lucas.lourenco@gmail.com>
2025-11-24 22:00:37 -0300

    fix: Sistema Papel json max depth cycle bug

--- SistemaAdministracao/Program.cs
+++ SistemaAdministracao/Program.cs
@@ -12,6 +12,13 @@ var senha = Environment.GetEnvironmentVariable("DB_PASSWORD");
 
 var connectionString = rawConnection.Replace("__DB_PASSWORD__", senha);
 
+builder.Services.AddControllers().AddJsonOptions(o =>
+{
+    o.JsonSerializerOptions.ReferenceHandler =
+        System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+});
+
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(connectionString));
 
